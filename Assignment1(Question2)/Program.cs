129efb55212aeca86cb7c1f07e47e4105c002fe0
Vishalpurkar100 <vishalpurkar100@gmail.com>
2024-05-15 12:23:10 +0530

    Assignment 1 Question 2

---
+++ Assignment1(Question2)/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,choice,c;
+            Console.WriteLine("1.Addition of two numbers :");
+            Console.WriteLine("2.Subtraction of two number :");
+            Console.WriteLine("3.Multiplication of two number :");
+            Console.WriteLine("4.Division of two number :");
+            Console.WriteLine("Enter the choice :");
+            choice = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter First Number :");
+            a=Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Enter second number :");
+            b=Convert.ToInt32( Console.ReadLine());
+           
+
+            
+           
+            switch (choice)
+            {
+                case 1:
+                    
+                    c = a + b;
+                    Console.WriteLine("Sum of two number is " + c);
+                    break;
+                    case 2: 
+                    c = a - b;
+                    Console.WriteLine("Subtraction of two number is " + c); 
+                    break;
+
+                    case 3:
+
+                    c=a*b;
+                    Console.WriteLine("multipication of two number is " + c);
+                    break;
+                    case 4:
+                    if (b == 0) Console.WriteLine("Check the denominator ");
+                    else
+                    {
+                        c = a / b;
+
+                        Console.WriteLine("Division of two number is " + c);
+
+                    }
+                    break ;
+            }
+        }
+    }
+}
